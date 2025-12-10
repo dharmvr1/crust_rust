1a624ecdbbfb323405fcfda555121591cdf51f0d
dharmvr1 <dharmhsing@gmail.com>
2025-12-10 23:07:13 +0530

    macro

--- macros/enum_as_str_derive/src/lib.rs
+++ macros/enum_as_str_derive/src/lib.rs
@@ -1,20 +1,64 @@
-extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use std::error;
-use syn::{Data, DeriveInput, parse_macro_input};
+use syn::{DataEnum, DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(AsStr)]
-pub fn derive_as_str(input: TokenStream) -> TokenStream {
+pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    let name = ast.ident;
+    let enum_name = ast.ident;
 
-    let variants =match ast.data {
-        Data::Enum(e) => e.variants,
-        _ => {syn::Error::new_spanned(name, "AsStr can only be derived for enums")
-            .to_compile_error()
-            .into()},
+    let variants = match ast.data {
+        syn::Data::Enum(e) => e.variants,
+        _ => {
+            return syn::Error::new_spanned(enum_name, "only for enums")
+                .into_compile_error()
+                .into();
+        }
+    };
+
+    let mut as_str_arr = Vec::new();
+    let mut from_str_arr = Vec::new();
+
+    for variant in variants {
+        let v_ident = variant.ident;
+        let v_str = v_ident.to_string();
+        as_str_arr.push(quote! {
+            #enum_name::#v_ident => #v_str
+        });
+
+        from_str_arr.push(quote! {
+            #v_str => Ok(#enum_name::#v_ident)
+        });
     }
 
-    let expand = quote! {}
+    let expand = quote! {
+        impl #enum_name{
+            fn as_str(&self)-> &str{
+                match self{
+
+
+                #(#as_str_arr),*}
+            }
+        }
+
+
+        impl std::fmt::Display for #enum_name{
+        fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+        }
+
+
+        impl std::str::FromStr for #enum_name{
+             type Err =();
+            fn from_str(s: &str) -> Result<Self, Self::Err>{
+               
+                 match s {
+                    #(#from_str_arr),*,
+                    _ => Err(())                 }
+            }
+        }
+    };
+   expand.into()
+
 }
--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,13 +1,3 @@
-use enum_as_str_derive::AsStr;
-
-#[derive(AsStr)]
-enum Color {
-    Red,
-    Green,
-    Blue,
-}
-
 fn main() {
-    let c = Color::Red;
-    println!("{}", c.as_str());
+    
 }
